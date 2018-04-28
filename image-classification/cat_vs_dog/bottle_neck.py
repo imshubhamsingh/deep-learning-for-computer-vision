3da245941b5c019b488e2e06997c042e2cfece3e
Shubham <imshubhamsingh97@gmail.com>
2018-04-28 23:39:42 +0530

    transfer learning

---
+++ image-classification/cat_vs_dog/bottle_neck.py
@@ -0,0 +1,64 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+work_dir = os.path.dirname(os.path.realpath(__file__))
+
+image_height, image_width = 150, 150
+train_dir = os.path.join(work_dir, 'data', 'train')
+test_dir = os.path.join(work_dir, 'data', 'test')
+no_classes = 2
+no_validation = 800
+epochs = 50
+batch_size = 32
+no_train = 2000
+no_test = 800
+input_shape = (image_height, image_width, 3)
+epoch_steps = no_train // batch_size
+test_steps = no_test // batch_size
+
+generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
+
+model = tf.keras.applications.VGG16(include_top=False)
+
+train_image = generator.flow_from_directory(
+    train_dir,
+    batch_size=batch_size,
+    target_size=(image_width, image_height),
+    class_mode=None,
+    shuffle=False
+)
+
+train_bottleneck_features = model.predict_generator(train_image, epoch_steps)
+
+
+test_images = generator.flow_from_directory(
+    test_dir,
+    batch_size=batch_size,
+    target_size=(image_width, image_height),
+    class_mode=None,
+    shuffle=False
+)
+
+test_bottleneck_features = model.predict_generator(test_images, test_steps)
+
+
+train_labels = np.array([0] * int(no_train / 2) + [1] * int(no_train / 2))
+test_labels = np.array([0] * int(no_test / 2) + [1] * int(no_test / 2))
+
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=train_bottleneck_features.shape[1:]))
+model.add(tf.keras.layers.Dense(1024, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Dense(1, activation='softmax'))
+model.compile(loss=tf.keras.losses.categorical_crossentropy,
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+model.fit(
+    train_bottleneck_features,
+    train_labels,
+    batch_size=batch_size,
+    epochs=epochs,
+    validation_data=(test_bottleneck_features, test_labels))
